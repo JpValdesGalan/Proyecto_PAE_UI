7b07df2eb4d0e78c857f310022cb35c75ebeb332
CarlosA <carlos.andres_paez@outlook.com>
2022-05-07 14:30:13 -0500

    Home with counter for entities

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CountersService} from '../../shared/services/counters.service'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  numberForums: Number = 0;
+  numberUsers: Number = 0;
+  numberReplies: Number = 0;
+  numberTopics: Number = 0;
+
+  constructor(private counterService: CountersService) { }
 
   ngOnInit(): void {
+    this.counterService.countComments().subscribe(result => {
+      console.log(result);
+      this.numberReplies = result.count;
+    });
+    this.counterService.countForums().subscribe(result => {
+      this.numberForums = result.count;
+    });
+    this.counterService.countTags().subscribe(result => {
+      this.numberTopics = result.count;
+    });
+    this.counterService.countUsers().subscribe(result => {
+      this.numberUsers = result.count;
+    });
   }
 
 }
---
+++ src/app/shared/services/counters.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountersService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  countUsers(): Observable <any>{
+    const counterURL = environment.BackendURL + '/users/count';
+    return this.httpClient.get(counterURL, {responseType: 'json'});
+  }
+
+  countForums(): Observable <any>{
+    const counterURL = environment.BackendURL + '/forums/count';
+    return this.httpClient.get(counterURL, {responseType: 'json'});
+  }
+
+  countTags(): Observable <any>{
+    const counterURL = environment.BackendURL + '/tags/count';
+    return this.httpClient.get(counterURL, {responseType: 'json'});
+  }
+
+  countComments(): Observable <any>{
+    const counterURL = environment.BackendURL + '/comments/count';
+    return this.httpClient.get(counterURL, {responseType: 'json'});
+  }
+}
